9c323f5ad984020cebf94d926a1b88c4f90f11d4
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-08 23:23:32 +0600

    using useReducer hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { useState } from 'react';
 import './App.css'
 import UseStateExample from './pages/UseStateExample.tsx';
+import UseReducer from './pages/UseReducer.tsx';
 
 function App() {
   const [counter, setCounter] = useState(0)
   return (
     <div>
-      <UseStateExample counter={counter} setCounter={setCounter} />
+      {/* <UseStateExample counter={counter} setCounter={setCounter} /> */}
+      <UseReducer></UseReducer>
     </div>
   )
 }
---
+++ src/pages/UseReducer.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-duplicate-case */
+import { ChangeEvent, useReducer } from "react";
+
+type TAction = {
+    type: string;
+    payload: string
+}
+
+
+const UseReducer = () => {
+    const initialState = { name: "", email: "" }
+    const reducer = (currentState: typeof initialState, action: TAction) => {
+
+        switch (action.type) {
+            case 'addName':
+                return { ...currentState, name: action.payload }
+            case 'addEmail':
+                return { ...currentState, email: action.payload }
+            case 'incrementBySetAmount':
+                return
+
+            default:
+                return currentState;
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(state)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={(e) => dispatch({ type: 'addName', payload: e.target.value })} type="text" name="name" id="name" />
+                <input onChange={(e) => dispatch({ type: 'addEmail', payload: e.target.value })} type="text" name="email" id="email" />
+                <button type="submit">Submit</button>
+            </form>
+
+            {/* <h1>{state.count}</h1>
+            <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'incrementBySetAmount', payload: 3 })}>Increment BY 3</button> */}
+        </div>
+    );
+};
+
+export default UseReducer;
