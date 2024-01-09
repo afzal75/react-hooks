bfa7975b07c642c89baabe702ab2df3f9307173f
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-09 12:31:17 +0600

    useEffect cleanUp

--- src/pages/UseEffect.tsx
+++ src/pages/UseEffect.tsx
@@ -12,21 +12,39 @@ import { useEffect, useState } from "react";
 const UseEffect = () => {
 
     const [hidden, setHidden] = useState(false)
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
-        console.log("Render")
-
-        return () => {
-            console.log("inside clean up")
-        }
-
-    }, [hidden])
+        setInterval(() => {
+            console.log("Render")
+            setCount((prev) => prev + 1)
+        }, 1000)
+    }, [])
 
     return (
         <div>
+            <h1>{count}</h1>
             <button onClick={() => setHidden((prev) => !prev)}>Click</button>
+            {!hidden && <Counter />}
         </div>
     );
 };
+
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        const interValID = setInterval(() => {
+            console.log("Render")
+            setCount((prev) => prev + 1)
+        }, 1000)
+        return () => {
+            clearInterval(interValID)
+        }
+    }, [])
+
+    return <h1>This is Value for: {count}</h1>
+}
+
 
 export default UseEffect;
