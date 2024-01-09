4c4e26beb40f7d6a284e85df28093969cc7e1410
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-09 13:24:37 +0600

    use effect dependency array explaination done

--- src/pages/UseEffect.tsx
+++ src/pages/UseEffect.tsx
@@ -12,22 +12,30 @@ import { useEffect, useState } from "react";
 
 const UseEffect = () => {
 
-    const [hidden, setHidden] = useState(false)
-    const [count, setCount] = useState(0);
-
+    // const [hidden, setHidden] = useState(false)
+    // const [count, setCount] = useState(0);
+    const [user, setUser] = useState({ name: "", email: "" })
+    console.log(user)
     useEffect(() => {
-        // setInterval(() => {
-        //     console.log("Render")
-        //     setCount((prev) => prev + 1)
-        // }, 1000)
-    }, [])
+        console.log("Render")
+    }, [user.name, user.email])
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={() => setHidden((prev) => !prev)}>Click</button>
-            {!hidden && <ToDo />}
+            <input className="border border-green-950" onBlur={(e) => setUser({ ...user, name: e.target.value })} type="text"
+                name="name"
+                id="name"
+            />
+            <input className="border border-red-700" onBlur={(e) => setUser({ ...user, email: e.target.value })} type="text"
+                name="email"
+                id="email"
+            />
         </div>
+        // <div>
+        //     <h1>{count}</h1>
+        //     <button onClick={() => setHidden((prev) => !prev)}>Click</button>
+        //     {!hidden && <ToDo />}
+        // </div>
     );
 };
 
